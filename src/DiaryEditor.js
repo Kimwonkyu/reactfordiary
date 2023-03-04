4c757e71a954bec071854b100e8749985d392601
Kimwonkyu <wonguo84@gmail.com>
2023-03-05 02:12:37 +0900

    8. DiaryEditor.js: 효율성을 위해 author, content부분의 중복된 코드를 하나로 타내기위해 state를 선언

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -2,6 +2,13 @@
 import {useState} from "react";
 
 const DiaryEditor =() => {
+    //*** 앞으로 동작이 비슷한 state끼리 묶을 예정이다. 효율성을 위해 아래 코드 작성***
+    const [state, setState] = useState({
+       author:"",
+       content:""
+    });
+
+
     //1. 작성자를 넣기 위해 선언 (input 테그에 작성자를 넣기 위해 author를 지정 그리고 상태변화를 나타내기 위해 setAuthor 지정
     const [author, setAuthor] = useState("");
     const [content,setContent] = useState("");//본문에 작성할 내용을 위해 content변수 선언
@@ -12,14 +19,14 @@ const DiaryEditor =() => {
         <div>
             <input
                    name="author"
-                   value={author}
+                   value={state.author}
                    onChange={(e) =>{
                    setAuthor(e.target.value);//상태를 그 값이 변경 될때마다 업데이트
             }
             }/>
         </div>
         <div>
-            <textarea value={content}
+            <textarea value={state.content}
                    onChange={(e) =>{
                    setContent(e.target.value);
             }
