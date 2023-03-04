2381c6e9a8bf8b3e717ea38e44817401e3da0a1f
Kimwonkyu <wonguo84@gmail.com>
2023-03-05 01:17:19 +0900

    4. DiaryEditor.js , input태그에 이벤트(e)를 받기위 작성 이벤트가 발생하는 시점을 onChange로 작성

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -3,12 +3,15 @@ import {useState} from "react";
 
 const DiaryEditor =() => {
     //1. 작성자를 넣기 위해 선언 (input 테그에 작성자를 넣기 위해 author를 지정 그리고 상태변화를 나타내기 위해 setAuthor 지정
-    const [author, setAuthor] = useState("");
+    const [author, setAuthor] = useState("DAN");
     //className을 최상위 태크와 맞춰준다 (찾기 편하라고..CSS)
     return <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
         <div>
-            <input value={author}/>
+            <input value={author} onChange={(e) =>{
+                console.log(e);
+            }
+            }/>
         </div>
     </div>;
 };
