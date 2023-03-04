659df7580726c43a7d25f5ad6934db32283beae1
Kimwonkyu <wonguo84@gmail.com>
2023-03-05 02:19:56 +0900

    9. DiaryEditor.js: state로 선언한 값들을 가져오기 위해 호출부의 코드를 변경함

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -8,10 +8,10 @@ const DiaryEditor =() => {
        content:""
     });
 
-
+    //처음 선언부 삭제
     //1. 작성자를 넣기 위해 선언 (input 테그에 작성자를 넣기 위해 author를 지정 그리고 상태변화를 나타내기 위해 setAuthor 지정
-    const [author, setAuthor] = useState("");
-    const [content,setContent] = useState("");//본문에 작성할 내용을 위해 content변수 선언
+    //const [author, setAuthor] = useState("");
+    //const [content,setContent] = useState("");//본문에 작성할 내용을 위해 content변수 선언
 
     //className을 최상위 태크와 맞춰준다 (찾기 편하라고..CSS)
     return <div className="DiaryEditor">
@@ -21,14 +21,27 @@ const DiaryEditor =() => {
                    name="author"
                    value={state.author}
                    onChange={(e) =>{
-                   setAuthor(e.target.value);//상태를 그 값이 변경 될때마다 업데이트
+                   setState({
+                       //author가 변하니까 타켓의 벨류가 넘어온다
+                       author: e.target.value,
+                       //content는 변함이 없으니 state의 content이다.
+                       content: state.content,
+                   });
+                   //아래 호출부는 삭제하고 setState로 변경
+                   //setAuthor(e.target.value);//상태를 그 값이 변경 될때마다 업데이트
             }
             }/>
         </div>
         <div>
             <textarea value={state.content}
                    onChange={(e) =>{
-                   setContent(e.target.value);
+                   setState({
+                       //여기는 content가 변하는 부분이기 때문에 author는 그대로 둠
+                       author: state.author,
+                       content: e.target.value
+                   });
+                   //아래 호출부는 삭제하고 setState로 변경
+                   //setContent(e.target.value);
             }
             }/>
 
