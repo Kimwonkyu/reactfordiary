aa4880fd9b8a277dd77a45e06bf96daa66e73e00
Kimwonkyu <wonguo84@gmail.com>
2023-03-05 23:57:18 +0900

    21. DiaryItem.js 을 생성하여 리스트 내용을 가지고 있도록 하고     이것을 DiaryList.js 에 prop으로 넘기도록 함
        DiaryList.js 에서는 기존에 dummy로 받아오는 부분을
        변경하여 DiaryItem.js의 값을 셋팅하도록 변경

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,3 +1,4 @@
+import DiaryItem from "./DiaryItem";
 
 const DiaryList = ({diaryList}) => {
     console.log(diaryList);
@@ -8,12 +9,14 @@ const DiaryList = ({diaryList}) => {
             <div>
                 {/*리액트에서는 .map을 리스트에서 사용하여 JSX 요소 목록을 랜더링할 때 유용하게 사용*/}
                 {diaryList.map((it)=>(
-                    <div key={it.id}>
+                    /*리스트는 이제 아이템에서 받아오도록 변경, 내용은 스프레드 연산자를 이용하여 모두 받음*/
+                  <DiaryItem key={it.id} {...it}/>
+                /*<div key={it.id}>
                         <div>작성자 : {it.author}</div>
                         <div>일기 : {it.content}</div>
                         <div>감정 : {it.emotion}</div>
                         <div>작성시간(ms) : {it.created_date}</div>
-                    </div>
+                    </div>*/
                 ))}
             </div>
         </div>
