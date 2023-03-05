bb948e09c6ec8bc091f327784e3e7e6ddad2e8d2
Kimwonkyu <wonguo84@gmail.com>
2023-03-05 23:38:53 +0900

    18. App.js에 더미 리스트를 만들어서     DiaryList.js 에 prop으로 받아와서     화면에 뿌려주도록 작성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,51 @@ import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
+//우선은 dummy로 List를 만들어 일기 리스트에 뿌려본다.
+const dummyList = [
+    {
+        id : 1,
+        author : "댄댄댄",
+        content : "하이",
+        emotion: 5,
+        created_date: new Date().getTime()
+    },
+    {
+        id : 2,
+        author : "댄댄댄2",
+        content : "하이2",
+        emotion: 2,
+        created_date: new Date().getTime()
+    },
+    {
+        id : 3,
+        author : "댄댄댄3",
+        content : "하이3",
+        emotion: 4,
+        created_date: new Date().getTime()
+    },
+    {
+        id : 4,
+        author : "댄댄댄4",
+        content : "하이4",
+        emotion: 1,
+        created_date: new Date().getTime()
+    },
+    {
+        id : 5,
+        author : "댄댄댄5",
+        content : "하이5",
+        emotion: 5,
+        created_date: new Date().getTime()
+    }
+]
+
+
 const App =() => {
   return (
       <div className="App">
         <DiaryEditor/>
-        <DiaryList/>
+        <DiaryList diaryList={dummyList}/>{/*dummyList를 prop으로 전달*/}
       </div>
   )
 }
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,9 +1,23 @@
 
-const DiaryList = () => {
-    return <div className="DiaryList">
-        <h2>일기 리스트</h2>
-    </div>
-
-}
+const DiaryList = ({diaryList}) => {
+    console.log(diaryList);
+    return (
+        <div className="DiaryList">
+            <h2>일기 리스트</h2>
+            <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {/*리액트에서는 .map을 리스트에서 사용하여 JSX 요소 목록을 랜더링할 때 유용하게 사용*/}
+                {diaryList.map((it)=>(
+                    <div>
+                        <div>작성자 : {it.author}</div>
+                        <div>일기 : {it.content}</div>
+                        <div>감정 : {it.emotion}</div>
+                        <div>작성시간(ms) : {it.created_date}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+        );
+};
 
 export default DiaryList;
